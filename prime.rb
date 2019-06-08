ce47fce41b4662d29ccb4a16f09f8a2bea1dd1fc
Mark Anthony Bagasol <markbagasol@gmail.com>
2019-06-08 03:16:50 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,1 @@
 # Add  code here!
-def prime?(num)
-    2.upto(Math.sqrt(num).ceil) do |i|
-        break if num%i==0
-        return true if i==Math.sqrt(num).ceil   
-    end
-    return false
-end
